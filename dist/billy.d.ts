444f62445941af2b52742de41c12dcc5e1cc333e
Gary Großgarten <gary.grossgarten@gmail.com>
2018-12-30 03:11:11 +0100

    init and release 0.1.0

---
+++ dist/billy.d.ts
@@ -0,0 +1,4 @@
+import { LaneContext } from "@fivethree/billy-core";
+export declare class ExampleApplication {
+    hello_world({ print }: LaneContext): void;
+}
---
+++ src/billy.ts
@@ -0,0 +1,11 @@
+import { App, Lane, LaneContext } from "@fivethree/billy-core";
+
+@App()
+export class ExampleApplication {
+
+    @Lane('This is an example lane.\nThe only thing it really does is output Hello World! 👾')
+    hello_world({ print }: LaneContext) {
+        print('Hello World');
+    }
+
+}
